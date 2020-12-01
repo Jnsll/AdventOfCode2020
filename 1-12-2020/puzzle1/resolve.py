c76649fd2d4ca2ba3e908a6cedc2239eb2dfd420
Jnsll <june.benvegnu-sallou@irisa.fr>
2020-12-01 09:30:20 +0100

    first day resolves; 1st draft, no opti

---
+++ 1-12-2020/puzzle1/resolve.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+
+expense_report = pd.read_csv("input", 'r', header=None)
+expense_report.columns = ["expense"]
+print(expense_report)
+
+
+for i in range(len(expense_report)):
+    for j in range(i+1, len(expense_report)):
+        if expense_report.iloc[i, 0] + expense_report.iloc[j, 0] == 2020:
+                answer = expense_report.iloc[i, 0] * expense_report.iloc[j, 0]
+                print(expense_report.iloc[i, 0], expense_report.iloc[j, 0])
+                break
+
+
+print(answer)
